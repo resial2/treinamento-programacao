afc2c1b9ad949778c1c43d3ca3303ae198ecd16f
João Marcelo <joaomarcelocustodio69@gmail.com>
2022-11-05 19:27:27 -0300

    adicionei a funcionalidade de divisão e subtração.

--- 001.py
+++ 001.py
@@ -1,4 +1,24 @@
+def div(n1, n2):
+    return n1 / n2
 
-nome = str(input("nome: "))
 
-print(nome)
+def sub(n1, n2):
+    return n1 - n2
+
+
+n1 = int
+n2 = int
+resl = int
+
+
+escl = int(input("digite 2 para divisão e 4 para subtração: "))
+if escl == 2:
+    n1 = int(input("digite o primeiro numero: "))
+    n2 = int(input("digite o segundo numero: "))
+    print(div(n1, n2))
+elif escl == 4:
+    n1 = int(input("digite o primeiro numero: "))
+    n2 = int(input("digite o segundo numero: "))
+    print(sub(n1, n2))
+
+
